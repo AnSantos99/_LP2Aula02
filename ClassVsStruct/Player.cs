db804b53bfda97eaa6d3e7e57ad4f63e64b126e0
AnSantos99 <an.santos95@hotmail.com>
2021-10-11 19:44:02 +0100

    Add class player to project

---
+++ ClassVsStruct/Player.cs
@@ -0,0 +1,9 @@
+namespace ClassVsStruct
+{
+    public class Player
+    {
+        public float Health {get; set;}
+
+        public float Armor {get; set;}
+    }
+}
---
+++ ClassVsStruct/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassVsStruct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare variables of type player
+            Player player1, player2;
+
+            // Initialize and set default values for health and armor properties
+            player1 = new Player() {Health = 100f, Armor = 150f};
+
+            // Point to the same reference
+            player2 = player1;
+
+            // Print out the result of each player instance
+            Console.WriteLine($"Player1:\tHealth:{player1.Health}\tArmor:{player1.Armor}");
+
+            Console.WriteLine($"Player2:\tHealth:{player2.Health}\tArmor:{player2.Armor}");
+
+
+        }
+    }
+}
